92d044a31359e873e3aaf7fe0daa9b00e299eb09
apzxnz <69621959+apzxnz@users.noreply.github.com>
2022-04-18 12:11:38 +0300

    2 задача

---
+++ Workspace/Homework_3/ArrayMinMaxAverageRandom.java
@@ -0,0 +1,54 @@
+package Homework_3;
+
+import utils.ArraysUtils;
+import utils.ScannerUtils;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArrayMinMaxAverageRandom {
+    public static void main(String[] args) {
+
+        System.out.println("Введите размер массива:");
+        int size = ScannerUtils.sc.nextInt();
+        int[] arr = ArraysUtils.createAndFillArray(size);
+        System.out.println(Arrays.toString(arr) + "\n");
+        if (size > 0) {
+            System.out.println(findMinMaxAverageNumber(arr));
+        } else {
+            System.out.println("Размер массива должен быть больше 0");
+        }
+        
+    }
+
+    private static Map<String, Number> findMinMaxAverageNumber(int[] arr) {
+        int min = 0, max = 0, sum = 0;
+        double average = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            if (i == 0) {
+                max = arr[i];
+                min = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            sum += arr[i];
+            average = sum / arr.length;
+        }
+        int finalMin = min, finalMax = max;
+        double finalAverage = average;
+
+        return new HashMap<>() {
+            {
+                put("min", finalMin);
+                put("max", finalMax);
+                put("average", finalAverage);
+            }
+        };
+    }
+}
+
