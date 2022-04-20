5c86bc8e3243e0ce2f165cbc8b32bb9c5d99cbc4
apzxnz <69621959+apzxnz@users.noreply.github.com>
2022-04-20 14:28:46 +0300

    1 задача в домашке 4

---
+++ Workspace/Homework_4/Array3D.java
@@ -0,0 +1,29 @@
+package Homework_4;
+
+import utils.ArraysUtils;
+import utils.ScannerUtils;
+
+import java.util.Arrays;
+
+public class Array3D {
+    public static void main(String[] args) {
+        
+        int[][][] arr3D = ArraysUtils.createAndFill3DArray();
+        System.out.println(Arrays.deepToString(arr3D));
+        System.out.println("Введите число на которое хотите увеличить каждый элемент массива:");
+        int number = ScannerUtils.sc.nextInt();
+        arr3D = increaseElementValueIn3DArray(arr3D, number);
+        System.out.println(Arrays.deepToString(arr3D));
+    }
+
+    public static int[][][] increaseElementValueIn3DArray(int[][][] arr3D, int number) {
+        for (int i = 0; i < arr3D.length ; i++) {
+            for (int j = 0; j < arr3D[0].length; j++) {
+                for (int k = 0; k < arr3D[0][0].length; k++) {
+                    arr3D[i][j][k] += number;
+                }
+            }
+        }
+        return arr3D;
+    }
+}
--- Workspace/utils/ArraysUtils.java
+++ Workspace/utils/ArraysUtils.java
@@ -11,4 +11,18 @@ public class ArraysUtils {
         }
         return arr;
     }
+
+    public static int[][][] createAndFill3DArray() {
+        Random generator = new Random(15);
+        int[][][] arr3D = new int[2][2][2];
+
+        for (int i = 0; i < arr3D.length ; i++) {
+            for (int j = 0; j < arr3D[0].length; j++) {
+                for (int k = 0; k < arr3D[0][0].length; k++) {
+                    arr3D[i][j][k] = generator.nextInt(10) + 1;
+                }
+            }
+        }
+        return arr3D;
+    }
 }
