d43f7dcf48c6e773b36e6ad5508271fd272c3d4a
apzxnz <69621959+apzxnz@users.noreply.github.com>
2022-06-04 13:24:28 +0300

    Revert "задача с исключениями"
    This reverts commit beaeca7d32a460d142d8ca01b079306eb76a3161.

--- Workspace/homework11/exceptions/FiveElementsLimitException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class FiveElementsLimitException extends Exception {
-
-    @Override
-    public String getMessage() {
-        return "Превышение лимита в 5 элементов";
-    }
-}
--- Workspace/homework11/exceptions/GetFromEmptyInputStorageException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class GetFromEmptyInputStorageException extends Exception {
-
-    @Override
-    public String getMessage() {
-        return "Память пуста";
-    }
-}
